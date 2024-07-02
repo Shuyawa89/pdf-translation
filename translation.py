c95adb74dae880a92a4ac0ddd960ec2eb5237512
Shuyawa89 <shuyaiwamoto@gmail.com>
2024-07-03 04:33:24 +0900

    change: modelを変更

--- translation.py
+++ translation.py
@@ -4,8 +4,9 @@ import re
 
 device = 0 if torch.cuda.is_available() else -1  # GPUが使用可能な場合は0、そうでない場合は-1
 
-translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-jap", device=device)
-# translator = pipeline('translation', model='staka/fugumt-en-ja', device=device)
+# translator = pipeline("translation", model="staka/fugumt-en-ja", device=device) # このモデルはなぜか文章がバグる
+model = "Hoax0930/marian-finetuned-kde4-en-to-ja_kftt"
+translator = pipeline("translation", model=model, device=device)
 
 def translate_text(text, num_splits=3):
     """
@@ -15,6 +16,7 @@ def translate_text(text, num_splits=3):
     @param num_splits: テキストを分割する数
     @return: 翻訳された日本語のテキスト
     """
+
     def split_text(text, num_splits):
         """テキストを指定した数に分割する。分割点は最も近い句読点で区切る。"""
         length = len(text)
@@ -36,6 +38,14 @@ def translate_text(text, num_splits=3):
             start = split_point
         splits.append(text[start:].strip())  # 残りのテキストを追加
         return splits
+    
+    def remove_extra_spaces(text):
+        """テキスト中の不要な空白を取り除く。"""
+        text = re.sub(r'\s+', ' ', text)  # 連続する空白を一つの空白に置換
+        text = re.sub(r'(\S) (\S)', r'\1\2', text)  # 単語間の空白を除去
+        text = re.sub(r'(\S) (\S)', r'\1\2', text)  # 単語間の空白を除去
+
+        return text
 
     translated_text = ""
     if len(text) > 250:
@@ -48,5 +58,7 @@ def translate_text(text, num_splits=3):
     
     # HTMLタグを除去
     translated_text = re.sub(r'<[^>]+>', '', translated_text)
+    # 不要な空白を取り除く
+    translated_text = remove_extra_spaces(translated_text)
     
     return translated_text
